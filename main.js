13b6c72d76ca24e3f3c63169842c3afa0f5edd6f
RamsesReyes12 <ramsesreyes01@gmail.com>
2025-11-05 22:08:45 -0500

    Added new event 'Concert' to events array
    Added a new event 'Concert' to the events array.

--- main.js
+++ main.js
@@ -3,5 +3,6 @@ const events = [
   { id: 1, name: "Music Festival", date: "2025-11-10", location: "New York" },
   { id: 2, name: "Tech Expo", date: "2025-12-05", location: "San Francisco" },
   { id: 3, name: "Comedy Night", date: "2025-11-22", location: "Chicago" },
-  { id: 4, name: "Art Exhibition", date: "2025-12-12", location: "Boston" }
+  { id: 4, name: "Art Exhibition", date: "2025-12-12", location: "Boston" }, 
+  { id: 5, name: "Concert", date: "2025-12-24", location: "Mexico City" }
 ];
